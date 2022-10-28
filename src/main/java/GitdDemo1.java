1490deae3ce7e2dfae899647c915a1cf4e78bad8
Liujiang <Liujiang@qq.com>
2022-10-28 10:54:21 +0800

    second commit

---
+++ src/main/java/GitdDemo1.java
@@ -0,0 +1,13 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+/**
+ * @Auther: liujiang
+ * @Date: 2022/10/28 - 10 - 28 - 10:51
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class GitdDemo1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
