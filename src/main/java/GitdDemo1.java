850658fb83c9a72049e8f3226c28a1b2c3bef5cc
Liujiang <Liujiang@qq.com>
2022-10-28 10:56:25 +0800

    third commit

--- src/main/java/GitdDemo1.java
+++ src/main/java/GitdDemo1.java
@@ -9,5 +9,6 @@ import com.sun.jdi.PathSearchingVirtualMachine;
 public class GitdDemo1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world12");
     }
 }
